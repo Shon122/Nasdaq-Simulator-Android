be11fe7d2b30f29edf0ffa6e5c18f326da22d6ac
Shon122 <ronencheckme@gmail.com>
2023-01-20 21:04:05 +0200

    Bank Rehovot

--- app/src/main/java/com/gtappdevelopers/bankrehovot/CurrencyPage.java
+++ app/src/main/java/com/gtappdevelopers/bankrehovot/CurrencyPage.java
@@ -33,27 +33,39 @@ public class CurrencyPage extends AppCompatActivity {
 //    EditText percentStopEditText = findViewById(R.id.percentStopEditText);
 //    CheckBox stopLossCheckBox = findViewById(R.id.stopLossCheckBox);
 //    CheckBox takeProfitCheckBox = findViewById(R.id.takeProfitCheckBox);
-    EditText amountInvestEditText = findViewById(R.id.amountInvestEditText);
-    EditText orderTradeEditText = findViewById(R.id.orderTradeEditText);
-    CheckBox orderTradeCheckBox = findViewById(R.id.orderTradeCheckBox);
-    TextView stockNameTextView = findViewById(R.id.stockName);
-    TextView userBalanceTextView = findViewById(R.id.userBalance);
-    TextView priceTextView = findViewById(R.id.currentPrice);
-    StockModel currentStock = MainActivity.viewingStock;
-    Double currentPrice = currentStock.priceList.get(0);
-    Double currentBalance = MainActivity.currentUser.balance;
-    Button longShortButton = findViewById(R.id.longShortButton);
-    boolean longShort = true;//true for long, false for short
+    EditText amountInvestEditText;
+    EditText orderTradeEditText;
+    CheckBox orderTradeCheckBox;
+    TextView stockNameTextView;
+    TextView userBalanceTextView;
+    TextView priceTextView;
+    StockModel currentStock;
+    Double currentPrice;
+    Double currentBalance;
+    Button longShortButton;
+    boolean longShort;//true for long, false for short
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.currency_page);
+        amountInvestEditText = findViewById(R.id.amountInvestEditText);
+        orderTradeEditText = findViewById(R.id.orderTradeEditText);
+        orderTradeCheckBox = findViewById(R.id.orderTradeCheckBox);
+        stockNameTextView = findViewById(R.id.stockName);
+        userBalanceTextView = findViewById(R.id.userBalance);
+        priceTextView = findViewById(R.id.currentPrice);
+        currentStock = MainActivity.viewingStock;
+        currentPrice = currentStock.priceList.get(0);
+        currentBalance = MainActivity.currentUser.balance;
+        longShortButton = findViewById(R.id.longShortButton);
+        longShort = true;//true for long, false for short
+
         priceTextView.setText(String.valueOf(currentPrice));
         stockNameTextView.setText(currentStock.name);
         userBalanceTextView.setText("Your Balance: " + currentBalance);
-
+orderTradeEditText.setVisibility(View.INVISIBLE);
     }
 
     @SuppressLint("SetTextI18n")
@@ -77,10 +89,11 @@ public class CurrencyPage extends AppCompatActivity {
                     currentStock.priceList.get(0), userAmount, -1.0, -1.0, longShort, true, orderPrice);
 
         }
-        userBalanceTextView.setText("" + (MainActivity.currentUser.balance - userAmount));
+        userBalanceTextView.setText("Your Balance: " + (MainActivity.currentUser.balance - userAmount));
         MainActivity.currentUser.balance = MainActivity.currentUser.balance - userAmount;
         MainActivity.currentUser.trades.add(newTrade);
         MainActivity.users.set(MainActivity.currentUserIndex, MainActivity.currentUser);
+        Toast.makeText(this, "Trade Created1", Toast.LENGTH_SHORT).show();
         MainActivity.uploadUsersToFirestore();
     }
 
